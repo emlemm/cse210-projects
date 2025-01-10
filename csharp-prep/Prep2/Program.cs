def3f5f0419315d3bdc34dd8ec08573ebad49659
emlemmon <77080516+emlemmon@users.noreply.github.com>
2025-01-09 23:20:47 -0700

    C# Prep 2 Core Requirement 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        int numberGrade = int.Parse(gradePercentage);
+
+        if (numberGrade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (numberGrade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (numberGrade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (numberGrade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
+    
