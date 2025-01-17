cfec03f68a79bc753a09e93eef386097c6b73876
emlemmon <77080516+emlemmon@users.noreply.github.com>
2025-01-16 22:14:36 -0700

    Completed Prep3 Core#2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -8,22 +8,25 @@ class Program
         string magicNumber = Console.ReadLine();
         int userNumber = int.Parse(magicNumber);
 
-        Console.Write("What is your guess? ");
-        string userGuess = Console.ReadLine();
-        int userGuessInt = int.Parse(userGuess);
-        if (userNumber < userGuessInt)
+        int userGuessInt;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            userGuessInt = int.Parse(userGuess);
+
+            if (userNumber < userGuessInt)
             {
                 Console.WriteLine("Lower");
             }
-
-        else if (userNumber > userGuessInt)
+            else if (userNumber > userGuessInt)
             {
                 Console.WriteLine("Higher");
             }
-
-        else
+            else
             {
                 Console.WriteLine("You guessed it!");
             }
+        } while (userGuessInt != userNumber);
     }
 }
