a9168338bf0dc053cb376d5e9e967050e271183e
emlemmon <77080516+emlemmon@users.noreply.github.com>
2025-01-16 22:00:33 -0700

    Complete Prep3 Core#1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int userNumber = int.Parse(magicNumber);
+
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int userGuessInt = int.Parse(userGuess);
+        if (userNumber < userGuessInt)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        else if (userNumber > userGuessInt)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        else
+            {
+                Console.WriteLine("You guessed it!");
+            }
     }
 }
