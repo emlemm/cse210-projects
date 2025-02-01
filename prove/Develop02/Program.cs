47f11e8f629ecc30ab3a4d1ece6517c2c8bd7cbf
emlemmon <77080516+emlemmon@users.noreply.github.com>
2025-01-31 22:22:21 -0700

    Program Class implemented

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,34 +6,84 @@ using System;
 using System.IO;
 class Program
 {
+    static Prompts _journalPrompts = new Prompts();
+    static Journal _currentJournal = new Journal();
     static void Main(string[] args)
     {
-        Prompts journalPrompts = new Prompts();
-        journalPrompts._promptList.Add("Who was the most interesting person I interacted with today?");
-        journalPrompts._promptList.Add("What was the best part of my day?");
-        journalPrompts._promptList.Add("How did I see the hand of the Lord in my life today?");
-        journalPrompts._promptList.Add("What was the strongest emotion I felt today?");
-        journalPrompts._promptList.Add("If I had one thing I could do over today, what would it be?");
-        journalPrompts._promptList.Add("What is one thing I accomplished today that I am proud of?");
-        journalPrompts._promptList.Add("What made today a good day?");
-        journalPrompts._promptList.Add("How was I a kind person today?");
-        journalPrompts._promptList.Add("How could I be a better person tomorrow?");
-        journalPrompts._promptList.Add("Who is somebody that I can help tomorrow?");
-        journalPrompts._promptList.Add("What was the best thing that I ate today?");
+        _journalPrompts._promptList.Add("Who was the most interesting person I interacted with today?");
+        _journalPrompts._promptList.Add("What was the best part of my day?");
+        _journalPrompts._promptList.Add("How did I see the hand of the Lord in my life today?");
+        _journalPrompts._promptList.Add("What was the strongest emotion I felt today?");
+        _journalPrompts._promptList.Add("If I had one thing I could do over today, what would it be?");
+        _journalPrompts._promptList.Add("What is one thing I accomplished today that I am proud of?");
+        _journalPrompts._promptList.Add("What made today a good day?");
+        _journalPrompts._promptList.Add("How was I a kind person today?");
+        _journalPrompts._promptList.Add("How could I be a better person tomorrow?");
+        _journalPrompts._promptList.Add("Who is somebody that I can help tomorrow?");
+        _journalPrompts._promptList.Add("What was the best thing that I ate today?");
         
-        // Testing
-        string userPrompt = journalPrompts.GetRandomPrompt();
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("Please select one of the following choices: ");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("What would you like to do? ");
+
+        string response = Console.ReadLine();
+        switch (response)
+        {
+            case "1": 
+                WriteEntry();
+                Menu();
+                break;
+            case "2":
+                _currentJournal.Display();
+                Menu();
+                break;
+            case "3":
+                LoadJournal();
+                Menu();
+                break;
+            case "4":
+                SaveJournal();
+                Menu();
+                break;
+            case "5":
+                return;
+        }
+    }
+
+    static void WriteEntry()
+    {        
+        Entry entry = new Entry();
+
+        string userPrompt = _journalPrompts.GetRandomPrompt();
         Console.WriteLine(userPrompt);
 
-        Entry testEntry = new Entry();
-        testEntry._prompt = userPrompt;
-        testEntry._entry = "My entry here. Yippee!";
-        testEntry._date = testEntry.GenerateDate();
-        testEntry.Display();
-        
-        // MENU
-        
-        //Console.WriteLine("What is the filename?");
-        //fileName = Console.ReadLine();
+        entry._entry = Console.ReadLine();
+        entry._prompt = userPrompt;
+        entry._date = entry.GenerateDate();
+
+        _currentJournal.AddEntry(entry);
+    }
+
+    static void LoadJournal()
+    {
+        Console.WriteLine("What is the filename?");
+        string fileName = Console.ReadLine();
+        _currentJournal = Journal.Load(fileName);
+    }
+
+    static void SaveJournal()
+    {
+        Console.WriteLine("What is the filename?");
+        string fileName = Console.ReadLine();
+        _currentJournal.SaveJournal(fileName);
     }
 }
