ba5674378e6c35e5880d717088cc2143f4b36790
emlemmon <77080516+emlemmon@users.noreply.github.com>
2025-02-06 21:52:16 -0700

    Learning03 Activity completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstFraction = new Fraction();
+        firstFraction.Display();
+        int top = firstFraction.GetTop();
+        Console.WriteLine(top);
+        firstFraction.SetTop(3);
+        int newTop = firstFraction.GetTop();
+        firstFraction.Display();
+
+        Fraction secondFraction = new Fraction(6);
+        secondFraction.Display();
+        secondFraction.SetBottom(4);
+        secondFraction.Display();
+
+        Fraction thirdFraction = new Fraction(6, 7);
+        thirdFraction.Display();
+        thirdFraction.SetTop(3);
+        thirdFraction.SetBottom(4);
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
+
+        Fraction fourthFraction = new Fraction(5);
+        Console.WriteLine(fourthFraction.GetFractionString());
+        Console.WriteLine(fourthFraction.GetDecimalValue());
+
     }
 }
